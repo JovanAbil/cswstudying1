58e216137bfa85f6643121b71aad396e9880c5d4
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-10-04 17:29:42 -0400

    Update rational-questions.ts

--- src/data/apprecalc/rational-questions.ts
+++ src/data/apprecalc/rational-questions.ts
@@ -27,7 +27,7 @@ export const rationalQuestions: Question[] = [
     "explanation": "3x^2/2x^2"
   },
   {
-    "id": "rational-",
+    "id": "rational-4",
     "type": "free-response",
     "question": "f(x) = (3x^2-1)/(2x^2+5x+7) Right End Behavior: ",
     "correctAnswer": "lim_x-->∞ f(x) = 3/2",
@@ -35,14 +35,14 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-5",
     "type": "free-response",
     "question": "g(x) = (3x^7+4x^2+7)/(x^2-3)^3 Left End Behavior: ",
     "correctAnswer": "lim_x-->-∞ g(x) = -∞",
     "explanation": "3x^7/x^6"
   },
   {
-    "id": "rational-",
+    "id": "rational-6",
     "type": "free-response",
     "question": "g(x) = (3x^7+4x^2+7)/(x^2-3)^3 Right End Behavior: ",
     "correctAnswer": "lim_x-->∞ g(x) = ∞",
@@ -50,14 +50,14 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-7",
     "type": "free-response",
     "question": "h(x) = (5x^3-2x^2-1)/(x^4-6) Left End Behavior",
     "correctAnswer": "lim_x-->-∞ h(x) = 0",
     "explanation": "5x^3/x^4"
   },
   {
-    "id": "rational-",
+    "id": "rational-8",
     "type": "free-response",
     "question": "h(x) = (5x^3-2x^2-1)/(x^4-6) Right End Behavior",
     "correctAnswer": "lim_x-->∞ h(x) = 0",
@@ -65,28 +65,28 @@ export const rationalQuestions: Question[] = [
   },
   //Terms
   {
-    "id": "rational-",
+    "id": "rational-9",
     "type": "free-response",
     "question": "What is a horizontal asymptote and how to find it on a rational expression?",
     "correctAnswer": "It is parallel to the x-axis and it is the highest degree terms on both the numerator and denominator divided by eachother.",
     "explanation": "N/A"
   },
   {
-    "id": "rational-",
+    "id": "rational-10",
     "type": "free-response",
     "question": "What is a vertical asymptote and how to find it on a rational expression?",
     "correctAnswer": "It is parallel to the y-axis and it is the factored expressions on the denominator",
     "explanation": "N/A"
   },
   {
-    "id": "rational-",
+    "id": "rational-11",
     "type": "free-response",
     "question": "What is a zero in a rational expression and how to find it on a rational expression?",
     "correctAnswer": "A zero is when y=0, it is found by the numerator on a rational expression",
     "explanation": "N/A"
   },
   {
-    "id": "rational-",
+    "id": "rational-12",
     "type": "free-response",
     "question": "What is a hole in a rational expression and how to find it on a rational expression?",
     "correctAnswer": "It is when an expression is fully factored on both the numerator and the denominator, find it by dividing the factors until both are gone.",
@@ -94,21 +94,21 @@ export const rationalQuestions: Question[] = [
   },
   //Asymptotes, Zeros, Holes Practice
   {
-    "id": "rational-",
+    "id": "rational-13",
     "type": "free-response",
     "question": "h(x) = (5x^3-2x^2-1)/(x^4-6) Horizontal Asymptote: ",
     "correctAnswer": "0",
     "explanation": "5x^3/x^4"
   },
   {
-    "id": "rational-",
+    "id": "rational-14",
     "type": "free-response",
     "question": "g(x) = (3x^7+4x^2+7)/(x^2-3)^3 Horizontal Asymptote: ",
     "correctAnswer": "None",
     "explanation": "3x^7/x^6, 3x, if x is on the numerator, its a linear 'asymptote (not correct term)' which means it isn't an asymptote"
   },
   {
-    "id": "rational-",
+    "id": "rational-15",
     "type": "free-response",
     "question": "f(x) = (3x^2-1)/(2x^2+5x+7) Horizontal Asymptote: ",
     "correctAnswer": "3/2",
@@ -116,21 +116,21 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-16",
     "type": "free-response",
     "question": "f(x) = (x^2+x-6)/(x^2+4x-5) Vertical Asymptote(s): ",
     "correctAnswer": "x = -5, 1",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-17",
     "type": "free-response",
     "question": "f(x) = (x^2+x-6)/(x^2+4x-5) Zero(s): ",
     "correctAnswer": "x = -3, 2",
     "explanation": "Factor out the top"
   },
   {
-    "id": "rational-",
+    "id": "rational-18",
     "type": "free-response",
     "question": "f(x) = (x^2+x-6)/(x^2+4x-5) Hole: ",
     "correctAnswer": "None",
@@ -138,21 +138,21 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-19",
     "type": "free-response",
     "question": "g(x) = (x^2-9x+18)/(x^2-8x+12) Vertical Asymptote(s): ",
     "correctAnswer": "x = 2",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-20",
     "type": "free-response",
     "question": "g(x) = (x^2-9x+18)/(x^2-8x+12) Zero(s): ",
     "correctAnswer": "x = 3",
     "explanation": "Factor out the top"
   },
   {
-    "id": "rational-",
+    "id": "rational-21",
     "type": "free-response",
     "question": "g(x) = (x^2-9x+18)/(x^2-8x+12) Hole: ",
     "correctAnswer": "x = 6",
@@ -160,21 +160,21 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-22",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Vertical Asymptote(s): ",
     "correctAnswer": "x = 1/2",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-23",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Zero(s): ",
     "correctAnswer": "x = 0, 8",
     "explanation": "Factor out the top"
   },
   {
-    "id": "rational-",
+    "id": "rational-24",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Hole: ",
     "correctAnswer": "x = -4",
@@ -182,21 +182,21 @@ export const rationalQuestions: Question[] = [
   },
   
   {
-    "id": "rational-",
+    "id": "rational-25",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Vertical Asymptote(s): ",
     "correctAnswer": "x = 1/2",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-26",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Zero(s): ",
     "correctAnswer": "x = 0, 8",
     "explanation": "Factor out the top"
   },
   {
-    "id": "rational-",
+    "id": "rational-27",
     "type": "free-response",
     "question": "h(x) = (x^3-4x^2-32x)/(2x^2+7x-4) Hole: ",
     "correctAnswer": "x = -4",
@@ -204,21 +204,21 @@ export const rationalQuestions: Question[] = [
   },
 
   {
-    "id": "rational-",
+    "id": "rational-28",
     "type": "free-response",
     "question": "j(x) = (x^2+3x-10)/(x^2-4x+4) Vertical Asymptote(s): ",
     "correctAnswer": "x = 2",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-29",
     "type": "free-response",
     "question": "j(x) = (x^2+3x-10)/(x^2-4x+4) Zero(s): ",
     "correctAnswer": "x = -5",
     "explanation": "Factor out the top"
   },
   {
-    "id": "rational-",
+    "id": "rational-30",
     "type": "free-response",
     "question": "j(x) = (x^2+3x-10)/(x^2-4x+4) Hole: ",
     "correctAnswer": "None",
@@ -226,21 +226,21 @@ export const rationalQuestions: Question[] = [
   },
 
   {
-    "id": "rational-",
+    "id": "rational-31",
     "type": "free-response",
     "question": "k(x) = (x-3)/(x^2+7x-30) Vertical Asymptote(s): ",
     "correctAnswer": "x = -10",
     "explanation": "Factor out the bottom"
   },
   {
-    "id": "rational-",
+    "id": "rational-32",
     "type": "free-response",
     "question": "k(x) = (x-3)/(x^2+7x-30) Zero(s): ",
     "correctAnswer": "None",
     "explanation": "Factors in top were factored out"
   },
   {
-    "id": "rational-",
+    "id": "rational-33",
     "type": "free-response",
     "question": "k(x) = (x-3)/(x^2+7x-30) Hole: ",
     "correctAnswer": "x = 3",
@@ -249,14 +249,14 @@ export const rationalQuestions: Question[] = [
 
   //Creating rational functions based on information
   {
-    "id": "rational-",
+    "id": "rational-34",
     "type": "free-response",
     "question": "Create a rational function with a hole at x = 3 and vertical asymptotes at x = 1 and x = -4",
     "correctAnswer": "Answers may vary; f(x) = (x-3)/(x-3)(x-1)(x+4)",
     "explanation": "N/A"
   },
   {
-    "id": "rational-",
+    "id": "rational-35",
     "type": "free-response",
     "question": "Create a rational function with zeros at x = -1 and x = 2, vertical asymptote at x = 5, and a horizontal asymptote of y=3/4",
     "correctAnswer": "Answers may vary; f(x) = 3(x+1)(x-2)/4(x-5)^2",
