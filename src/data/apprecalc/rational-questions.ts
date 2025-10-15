d205bda70219de1367e2c8792a59f8e5ff2de1aa
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-10-15 09:52:15 -0400

    Update rational-questions.ts

--- src/data/apprecalc/rational-questions.ts
+++ src/data/apprecalc/rational-questions.ts
@@ -7,7 +7,7 @@ export const rationalQuestions: Question[] = [
     "type": "free-response",
     "question": "Instructions on Image",
     "image": "/images/apprecalc/rational1.png",
-    "correctAnswer": "As the x-values increase without bound, the y-values approach but never reach -1",
+    "correctAnswer": "As the x-values decrease without bound, the y-values approach but never reach -1",
     "explanation": "The image shows the horizontal asymptote to be -1 which is what will never have a value but the function will try to keep approaching."
   },
   {
