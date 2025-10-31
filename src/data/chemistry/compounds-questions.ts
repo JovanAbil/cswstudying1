19933b4a76fbc7acf432261c56a5418553c5a385
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-10-31 08:53:54 -0400

    Update compounds-questions.ts

--- src/data/chemistry/compounds-questions.ts
+++ src/data/chemistry/compounds-questions.ts
@@ -1,3 +1,466 @@
 import { Question } from '@/types/quiz';
 
-export const compoundsQuestions: Question[] = [];
+export const compoundsQuestions: Question[] = [
+  {
+    id: 'compounds-1a',
+    type: 'free-response',
+    question: 'What is the name of NH4⁺?',
+    correctAnswer: 'ammonium',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-1b',
+    type: 'free-response',
+    question: 'What is the formula for ammonium?',
+    correctAnswer: 'NH4⁺',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-1c',
+    type: 'free-response',
+    question: 'What is the charge of ammonium?',
+    correctAnswer: '+1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-2a',
+    type: 'free-response',
+    question: 'What is the name of C2H3O2⁻?',
+    correctAnswer: 'acetate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-2b',
+    type: 'free-response',
+    question: 'What is the formula for acetate?',
+    correctAnswer: 'C2H3O2⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-2c',
+    type: 'free-response',
+    question: 'What is the charge of acetate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-3a',
+    type: 'free-response',
+    question: 'What is the name of ClO3⁻?',
+    correctAnswer: 'chlorate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-3b',
+    type: 'free-response',
+    question: 'What is the formula for chlorate?',
+    correctAnswer: 'ClO3⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-3c',
+    type: 'free-response',
+    question: 'What is the charge of chlorate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-4a',
+    type: 'free-response',
+    question: 'What is the name of ClO2⁻?',
+    correctAnswer: 'chlorite',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-4b',
+    type: 'free-response',
+    question: 'What is the formula for chlorite?',
+    correctAnswer: 'ClO2⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-4c',
+    type: 'free-response',
+    question: 'What is the charge of chlorite?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-5a',
+    type: 'free-response',
+    question: 'What is the name of CN⁻?',
+    correctAnswer: 'cyanide',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-5b',
+    type: 'free-response',
+    question: 'What is the formula for cyanide?',
+    correctAnswer: 'CN⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-5c',
+    type: 'free-response',
+    question: 'What is the charge of cyanide?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-6a',
+    type: 'free-response',
+    question: 'What is the name of ClO⁻?',
+    correctAnswer: 'hypochlorite',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-6b',
+    type: 'free-response',
+    question: 'What is the formula for hypochlorite?',
+    correctAnswer: 'ClO⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-6c',
+    type: 'free-response',
+    question: 'What is the charge of hypochlorite?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-7a',
+    type: 'free-response',
+    question: 'What is the name of HCO3⁻?',
+    correctAnswer: 'bicarbonate (hydrogen carbonate)',
+    explanation: 'Memorize'
+  },
+    {
+    id: 'compounds-7b',
+    type: 'free-response',
+    question: 'What is the formula for bicarbonate?',
+    correctAnswer: 'HCO3⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-7c',
+    type: 'free-response',
+    question: 'What is the charge of bicarbonate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-8a',
+    type: 'free-response',
+    question: 'What is the name of OH⁻?',
+    correctAnswer: 'hydroxide',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-8b',
+    type: 'free-response',
+    question: 'What is the formula for hydroxide?',
+    correctAnswer: 'OH⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-8c',
+    type: 'free-response',
+    question: 'What is the charge of hydroxide?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-9a',
+    type: 'free-response',
+    question: 'What is the name of NO3⁻?',
+    correctAnswer: 'nitrate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-9b',
+    type: 'free-response',
+    question: 'What is the formula for nitrate?',
+    correctAnswer: 'NO3⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-9c',
+    type: 'free-response',
+    question: 'What is the charge of nitrate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-10a',
+    type: 'free-response',
+    question: 'What is the name of NO2⁻?',
+    correctAnswer: 'nitrite',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-10b',
+    type: 'free-response',
+    question: 'What is the formula for nitrite?',
+    correctAnswer: 'NO2⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-10c',
+    type: 'free-response',
+    question: 'What is the charge of nitrite?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-11a',
+    type: 'free-response',
+    question: 'What is the name of ClO4⁻?',
+    correctAnswer: 'perchlorate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-11b',
+    type: 'free-response',
+    question: 'What is the formula for perchlorate?',
+    correctAnswer: 'ClO4⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-11c',
+    type: 'free-response',
+    question: 'What is the charge of perchlorate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-12a',
+    type: 'free-response',
+    question: 'What is the name of MnO4⁻?',
+    correctAnswer: 'permanganate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-12b',
+    type: 'free-response',
+    question: 'What is the formula for permanganate?',
+    correctAnswer: 'MnO4⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-12c',
+    type: 'free-response',
+    question: 'What is the charge of permanganate?',
+    correctAnswer: '-1',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-13a',
+    type: 'free-response',
+    question: 'What is the name of CO3²⁻?',
+    correctAnswer: 'carbonate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-13b',
+    type: 'free-response',
+    question: 'What is the formula for carbonate?',
+    correctAnswer: 'CO3²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-13c',
+    type: 'free-response',
+    question: 'What is the charge of carbonate?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-14a',
+    type: 'free-response',
+    question: 'What is the name of CrO4²⁻?',
+    correctAnswer: 'chromate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-14b',
+    type: 'free-response',
+    question: 'What is the formula for chromate?',
+    correctAnswer: 'CrO4²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-14c',
+    type: 'free-response',
+    question: 'What is the charge of chromate?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-15a',
+    type: 'free-response',
+    question: 'What is the name of Cr2O7²⁻?',
+    correctAnswer: 'dichromate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-15b',
+    type: 'free-response',
+    question: 'What is the formula for dichromate?',
+    correctAnswer: 'Cr2O7²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-15c',
+    type: 'free-response',
+    question: 'What is the charge of dichromate?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-16a',
+    type: 'free-response',
+    question: 'What is the name of O2²⁻?',
+    correctAnswer: 'peroxide',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-16b',
+    type: 'free-response',
+    question: 'What is the formula for peroxide?',
+    correctAnswer: 'O2²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-16c',
+    type: 'free-response',
+    question: 'What is the charge of peroxide?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-17a',
+    type: 'free-response',
+    question: 'What is the name of SO4²⁻?',
+    correctAnswer: 'sulfate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-17b',
+    type: 'free-response',
+    question: 'What is the formula for sulfate?',
+    correctAnswer: 'SO4²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-17c',
+    type: 'free-response',
+    question: 'What is the charge of sulfate?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-18a',
+    type: 'free-response',
+    question: 'What is the name of SO3²⁻?',
+    correctAnswer: 'sulfite',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-18b',
+    type: 'free-response',
+    question: 'What is the formula for sulfite?',
+    correctAnswer: 'SO3²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-18c',
+    type: 'free-response',
+    question: 'What is the charge of sulfite?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-19a',
+    type: 'free-response',
+    question: 'What is the name of PO4³⁻?',
+    correctAnswer: 'phosphate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-19b',
+    type: 'free-response',
+    question: 'What is the formula for phosphate?',
+    correctAnswer: 'PO4³⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-19c',
+    type: 'free-response',
+    question: 'What is the charge of phosphate?',
+    correctAnswer: '-3',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-20a',
+    type: 'free-response',
+    question: 'What is the name of PO3³⁻?',
+    correctAnswer: 'phosphite',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-20b',
+    type: 'free-response',
+    question: 'What is the formula for phosphite?',
+    correctAnswer: 'PO3³⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-20c',
+    type: 'free-response',
+    question: 'What is the charge of phosphite?',
+    correctAnswer: '-3',
+    explanation: 'Memorize'
+  },
+
+  {
+    id: 'compounds-21a',
+    type: 'free-response',
+    question: 'What is the name of C2O4²⁻?',
+    correctAnswer: 'oxalate',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-21b',
+    type: 'free-response',
+    question: 'What is the formula for oxalate?',
+    correctAnswer: 'C2O4²⁻',
+    explanation: 'Memorize'
+  },
+  {
+    id: 'compounds-21c',
+    type: 'free-response',
+    question: 'What is the charge of oxalate?',
+    correctAnswer: '-2',
+    explanation: 'Memorize'
+  },
+
+];
