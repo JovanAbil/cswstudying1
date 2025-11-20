6ea71ef86c0fad8dd97e4b0f0384840c341bdf0a
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-11-20 07:36:57 -0500

    Update general3-questions.ts

--- src/data/memory/general3-questions.ts
+++ src/data/memory/general3-questions.ts
@@ -1,5 +1,36 @@
 import { Question } from '@/types/quiz';
 
 export const general3Questions: Question[] = [
-  
+  {
+      "id": "memory3-1",
+      "type": "free-response",
+      "question": "What is the rate?",
+      "image": "/images/memory/general3-1.png",
+      "correctAnswer": "It is decreasing at an increasing rate",
+      "explanation": "Memorize"
+  },
+  {
+      "id": "memory3-2",
+      "type": "free-response",
+      "question": "What is the rate?",
+      "image": "/images/memory/general3-2.png",
+      "correctAnswer": "It is decreasing at an decreasing rate",
+      "explanation": "Memorize"
+  },
+  {
+      "id": "memory3-3",
+      "type": "free-response",
+      "question": "What is the rate?",
+      "image": "/images/memory/general3-3.png",
+      "correctAnswer": "It is increasing at an decreasing rate",
+      "explanation": "Memorize"
+  },
+  {
+      "id": "memory3-4",
+      "type": "free-response",
+      "question": "What is the rate?",
+      "image": "/images/memory/general3-4.png",
+      "correctAnswer": "It is increasing at an increasing rate",
+      "explanation": "Memorize"
+  },
 ];
