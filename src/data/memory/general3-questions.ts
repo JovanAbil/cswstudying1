f2c40015f51beb9d5edbe62d5f8c0c75b7b0ff0c
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-11-20 07:43:39 -0500

    Fix duplicate question entries in general3Questions

--- src/data/memory/general3-questions.ts
+++ src/data/memory/general3-questions.ts
@@ -4,32 +4,32 @@ export const general3Questions: Question[] = [
   {
       "id": "memory3-1",
       "type": "free-response",
-      "question": "What is the rate?",
       "image": "/images/memory/general3-1.png",
+      "question": "What is the rate?",
       "correctAnswer": "It is decreasing at an increasing rate",
       "explanation": "Memorize"
   },
   {
       "id": "memory3-2",
       "type": "free-response",
-      "question": "What is the rate?",
       "image": "/images/memory/general3-2.png",
+      "question": "What is the rate?",
       "correctAnswer": "It is decreasing at an decreasing rate",
       "explanation": "Memorize"
   },
   {
       "id": "memory3-3",
       "type": "free-response",
-      "question": "What is the rate?",
       "image": "/images/memory/general3-3.png",
+      "question": "What is the rate?",
       "correctAnswer": "It is increasing at an decreasing rate",
       "explanation": "Memorize"
   },
   {
       "id": "memory3-4",
       "type": "free-response",
-      "question": "What is the rate?",
       "image": "/images/memory/general3-4.png",
+      "question": "What is the rate?",
       "correctAnswer": "It is increasing at an increasing rate",
       "explanation": "Memorize"
   },
