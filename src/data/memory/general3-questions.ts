ed0ff775cce3886b2be73f3bac8ac8054ace3c52
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-11-20 10:59:15 -0500

    Fix correct answers for memory3 questions

--- src/data/memory/general3-questions.ts
+++ src/data/memory/general3-questions.ts
@@ -6,7 +6,7 @@ export const general3Questions: Question[] = [
       "type": "free-response",
       "image": "/images/memory/general3-1.png",
       "question": "What is the rate?",
-      "correctAnswer": "It is decreasing at an increasing rate",
+      "correctAnswer": "It is decreasing at an decreasing rate",
       "explanation": "Memorize"
   },
   {
@@ -14,7 +14,7 @@ export const general3Questions: Question[] = [
       "type": "free-response",
       "image": "/images/memory/general3-2.png",
       "question": "What is the rate?",
-      "correctAnswer": "It is decreasing at an decreasing rate",
+      "correctAnswer": "It is decreasing at an increasing rate",
       "explanation": "Memorize"
   },
   {
