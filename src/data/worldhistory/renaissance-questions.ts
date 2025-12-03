4506ab560c27bbbd126a9c49d783fff09e418643
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-12-02 20:24:58 -0500

    Update renaissance-questions.ts

--- src/data/worldhistory/renaissance-questions.ts
+++ src/data/worldhistory/renaissance-questions.ts
@@ -1,3 +1,36 @@
 import { Question } from '@/types/quiz';
 
-export const renaissanceQuestions: Question[] = [];
+export const renaissanceQuestions: Question[] = [
+  
+  {
+    id: 'renaissance-',
+    type: 'free-response',
+    question: '',
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: 'True' },
+      { label: 'B', value: 'B', text: 'False' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+  {
+    id: 'renaissance-',
+    type: 'multiple-choice',
+    question: '',
+    options: [
+      { label: 'A', value: 'A', text: '' },
+      { label: 'B', value: 'B', text: '' },
+      { label: 'C', value: 'C', text: '' },
+      { label: 'D', value: 'D', text: '' },
+    ],
+    correctAnswer: '',
+    explanation: ''
+  },
+];
