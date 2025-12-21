237a4246962b1c5caaa4aff887011ad4dbd53134
JovanAbil <164279478+JovanAbil@users.noreply.github.com>
2025-12-21 14:05:27 -0500

    Update exponential-questions.ts

--- src/data/apprecalc/exponential-questions.ts
+++ src/data/apprecalc/exponential-questions.ts
@@ -1,4 +1,387 @@
 //Damiani
 import { Question } from '@/types/quiz';
 
-export const exponentialQuestions: Question[] = [];
+export const exponentialQuestions: Question[] = [
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: "",
+    type: "multiple-choice",
+    question: "",
+    options: [
+      { "label": "A", "value": "A", "text": "" },
+      { "label": "B", "value": "B", "text": "" },
+      { "label": "C", "value": "C", "text": "" },
+      { "label": "D", "value": "D", "text": "" }
+    ],
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+  {
+    id: " ",
+    type: "free-response",
+    question: "",
+    correctAnswer: "",
+    explanation: ""
+  },
+];
